9f9f223a0b8840493e0e2afe6f669b17931e9d81
AnarZulfuqarov <anarrz-af106@code.edu.az>
2025-04-27 15:00:57 +0400

    Kliniken

--- src/components/UserComponents/Home/HotelCard/index.jsx
+++ src/components/UserComponents/Home/HotelCard/index.jsx
@@ -1,24 +1,31 @@
-import "./index.scss"
-import back from "/src/assets/ServiceDetailCard.png"
-import image from "/src/assets/Subtract.png"
-import {IoLocationOutline} from "react-icons/io5";
+import "./index.scss";
+import back from "/src/assets/ServiceDetailCard.png";
+import defaultImage from "/src/assets/Subtract.png"; // Varsayılan resim
+import hoverImage from "/src/assets/HoverImage.png"; // Hover sırasında gösterilecek resim
+import { IoLocationOutline } from "react-icons/io5";
+
 function HotelCard() {
     return (
         <div className={"col-3"}>
-            <div id={"hotelCard"} style={{
-                background:`linear-gradient(0deg, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${back})`,
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-
-            }}>
+            <div
+                id={"hotelCard"}
+                style={{
+                    background: `linear-gradient(0deg, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${back})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                }}
+            >
                 <div className={"image"}>
-                    <img src={image} />
+                    <img src={defaultImage} className="default-image" alt="default" />
+                    <img src={hoverImage} className="hover-image" alt="hover" />
                     <div className={"card-title"}>
-                        <p><IoLocationOutline className={"icon"}/>Almaniya</p>
+                        <p>
+                            <IoLocationOutline className={"icon"} />
+                            Almaniya
+                        </p>
                         <h5>Universitätsklinikum</h5>
                     </div>
                 </div>
-
             </div>
         </div>
     );
